2d2938a11bc1f26f3873657c91ea1f2ea199d639
myusername <jquiros90650@ufide.ac.cr>
2022-12-09 13:03:38 -0600

    Initial commit

---
+++ app/src/main/java/com/example/practicaexamenfinal/Activity/IntroActivity.kt
@@ -0,0 +1,25 @@
+package com.example.practicaexamenfinal.Activity;
+
+import android.os.Bundle
+import android.view.View
+import androidx.core.content.ContextCompat.startActivity
+import com.example.practicaexamenfinal.R
+
+
+public class IntroActivity extends AppCompatActivity {
+    private ConstraintLayout startBtn;
+
+    @override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+
+        startBtn=findViewById(R.id.startBtn);
+        startBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                startActivity(new Intent(IntroActivity.this,MainActivity.class));
+            }
+        })
+    }
+}
